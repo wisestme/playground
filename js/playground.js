ca2e544c3252a1beb790414bda0e6975a4584c46
wisestMe <wyhzest@gmail.com>
2019-11-19 13:57:03 +0100

    implements drag and drop

---
+++ js/playground.js
@@ -0,0 +1,34 @@
+let dragStart = function (e) {
+  // IE doesn't support text/plain
+  try {
+    e.dataTransfer.setData('text/plain', e.target.id);
+  } catch (ex) {
+    e.dataTransfer.setData('Text', e.target.id);
+  }
+}
+
+let cancel = function (e) {
+  if(e.preventDefault) e.preventDefault();
+  if(e.stopPropagation) e.stopPropagation();
+  return false;
+}
+
+let dropped = function (e) {
+let id;
+
+cancel(e);
+try {
+  id = e.dataTransfer.getData('text/plain');
+} catch (ex) {
+  id = e.dataTransfer.getData('Text');
+}
+  e.target.appendChild(document.querySelector('#' + id));
+};
+
+let musical = document.querySelector('#even-if');
+musical.addEventListener('dragstart', dragStart, false);
+
+let target = document.querySelector('#target-container');
+target.addEventListener('drop', dropped, false);
+target.addEventListener('dragenter', cancel, false);
+target.addEventListener('dragover', cancel, false);
