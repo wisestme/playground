fedcd63541149c5f482d09fc14ba46a210221990
wisestMe <wyhzest@gmail.com>
2020-02-02 17:02:35 +0100

    style list elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 //$('h1#heading1').hide();
 $('h1.heading2').hide();
-$('p span').css('color', '#f1a')
+$('p span').css('color', '#f1a');
+$('li:first-child').css('color', '#f1a');
+$('li:last-child').css('color', '#a1f');
+$('li:even').css('color', '#1af');
+
+
