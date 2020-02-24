e9c384b2931f846b0e4207d7a896a690af86bf85
wisestMe <wyhzest@gmail.com>
2020-02-24 08:17:27 +0100

    calculate total price

--- js/ydkjs_up_going.js
+++ js/ydkjs_up_going.js
@@ -1,9 +1,6 @@
+var TAX_RATE = 0.08;
 var amount = 99.99;
-amount = amount * 2;
+amount = amount + (amount * TAX_RATE);
 
-console.log(typeof amount);
-
-amount = '$' + String(amount);
-console.log(typeof amount);
-amount = String(amount);
-console.log(amount)
+console.log(amount);
+console.log(amount.toFixed(2));
