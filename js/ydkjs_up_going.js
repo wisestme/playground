f4cb3974739e1f8b229c479a52e4992e34079582
wisestMe <wyhzest@gmail.com>
2020-02-24 08:41:45 +0100

    add an else clause

--- js/ydkjs_up_going.js
+++ js/ydkjs_up_going.js
@@ -1,6 +1,11 @@
 var bank_balance = 302.13;
 var amount = 99.99;
+const ACCESSORY_PRICE = 9.99;
 
+amount *= 4;
 if (amount < bank_balance) {
 	console.log('I want to buy phone');
+	amount += ACCESSORY_PRICE;
+} else {
+	console.log('I do not want the accessory for now');
 }
