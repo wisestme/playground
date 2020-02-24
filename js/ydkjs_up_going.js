84293f6470dd87486725bb3686bb76fdebbbfe2b
wisestMe <wyhzest@gmail.com>
2020-02-24 08:09:35 +0100

    add new files

---
+++ js/ydkjs_up_going.js
@@ -0,0 +1,9 @@
+var amount = 99.99;
+amount = amount * 2;
+
+console.log(typeof amount);
+
+amount = '$' + String(amount);
+console.log(typeof amount);
+amount = String(amount);
+console.log(amount)
