fbf1718252faf64b3df4a4afbf9055cf6ff6e2f2
wisestMe <wyhzest@gmail.com>
2020-02-25 09:20:06 +0100

    implement a while loop

--- js/ydkjs_up_going.js
+++ js/ydkjs_up_going.js
@@ -1,11 +1,10 @@
-var bank_balance = 302.13;
-var amount = 99.99;
-const ACCESSORY_PRICE = 9.99;
+let numberOfCustomers = 10;
+while (numberOfCustomers > 0) {
+	numberOfCustomers -= 1;
+	while (numberOfCustomers > 0) {
+		console.log('please how may I help you');
+		numberOfCustomers -= 1;
+	}
+	
 
-amount *= 4;
-if (amount < bank_balance) {
-	console.log('I want to buy phone');
-	amount += ACCESSORY_PRICE;
-} else {
-	console.log('I do not want the accessory for now');
 }
