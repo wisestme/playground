fb4f72b9984fc013588e9a84a0081ed8f21b1461
wisestMe <wyhzest@gmail.com>
2020-03-02 23:14:49 +0100

    create objects

--- js/openclassroom.js
+++ js/openclassroom.js
@@ -1,9 +1,9 @@
-let myBook = {
-	title: 'The Last Outcast',
-	author: 'Chris Okotie',
-	numberOfPages: 500,
-	isAvailable: true
-};
+// let myBook = {
+// 	title: 'The Last Outcast',
+// 	author: 'Chris Okotie',
+// 	numberOfPages: 500,
+// 	isAvailable: true
+// };
 
 let episode = {
 	title: 'Magnificient 7',
@@ -12,3 +12,17 @@ let episode = {
 };
 
 console.log(episode.title);
+
+class Book {
+	constructor(title, author, pages) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+	}
+}
+
+let myBook = new Book('The Last Outcast', 'Chris Okotie', 500);
+let mySecondBook = new Book('One Way Love', 'Tulian Tchividian', 400);
+
+console.log(myBook, mySecondBook);
+
