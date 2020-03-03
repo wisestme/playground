adce4d9abafb51307404b6f6f233848aa3e42434
wisestMe <wyhzest@gmail.com>
2020-03-03 20:15:28 +0100

    register service worker

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-if (navigator.serviceWorker.controller) {
-	debug(
-    navigator.serviceWorker.controller.scriptURL +
-    ' (onload)', 'controller'
-  );
-  debug(
-    'An active service worker controller was found, ' +
-    'no need to register'
-  );
-} else {
-	navigator.serviceWorker.register('service-worker.js', {
-    scope: './'
-  }).then(function(reg) {
-    debug(reg.scope, 'register');
-    debug('Service worker change, registered the service worker');
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js');
   });
 }
