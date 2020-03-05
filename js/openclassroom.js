ffaf5b1471a668a25b6bf1c4c6893ea29a7a9c17
wisestMe <wyhzest@gmail.com>
2020-03-05 22:07:29 +0100

    pass objects into array

--- js/openclassroom.js
+++ js/openclassroom.js
@@ -1,5 +1,5 @@
-let firstGuest = 'Vincent Gloria';
-let secondGuest = 'Chijioke Nwagwu';
+let firstGuest = {name: 'Vincent Gloria', vip: true};
+let secondGuest = {name: 'Chijioke Nwagwu', vip: true};
 
 let guests = [firstGuest, secondGuest];
 
