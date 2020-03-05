5e0f0908b60c9be95ad631c1f66b6fa3cfcf9813
wisestMe <wyhzest@gmail.com>
2020-03-05 20:59:41 +0100

    create objects from constructor

--- js/openclassroom.js
+++ js/openclassroom.js
@@ -26,3 +26,22 @@ let mySecondBook = new Book('One Way Love', 'Tulian Tchividian', 400);
 
 console.log(myBook, mySecondBook);
 
+let firstGuest = {name: 'Vincent Gloria', vip: true};
+let secondGuest = {name: 'Chijioke Nwagwu', vip: true};
+
+let guests = [firstGuest, secondGuest];
+
+console.log(guests);
+
+class Song {
+	constructor(artist, album, duration, title) {
+		this.artist = artist;
+		this.album = album;
+		this.duration = duration;
+		this.title = title;
+	}
+}
+
+let livingHope = new Song('Phil Wickham', 'King Jesus', 240, 'Living Hope');
+let wholeHeart = new Song('Brandon Heath', 'Jesus Cares', 260, 'Whole Heart');
+console.log(livingHope.artist);
