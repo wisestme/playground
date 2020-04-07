c43be3035a4220e254a99049ef4af54ad3ce0138
wisestMe <wyhzest@gmail.com>
2020-04-07 06:15:09 +0100

    add carousel html

--- js/ydkjs_up_going.js
+++ js/ydkjs_up_going.js
@@ -1,38 +1,51 @@
-let phonePrice = [80, 200, 220, 90, 100];
-let accessoryPrice = [50, 120, 125, 55, 60];
-let wallet = prompt('How much do you have in your wallet?');
-let balance = [];
-let purchase = [];
-const TAX_RATE = 0.08;
+// let phonePrice = [80, 200, 220, 90, 100];
+// let accessoryPrice = [50, 120, 125, 55, 60];
+// let wallet = prompt('How much do you have in your wallet?');
+// let balance = [];
+// let purchase = [];
+// const TAX_RATE = 0.08;
 
-for(let i = 0; i < phonePrice.length; i++) {
-	let total = phonePrice[i] + accessoryPrice[i];
-	if(phonePrice[i] < wallet) {
-		wallet = wallet - phonePrice[i];
-		if(accessoryPrice[i] < wallet) {
-			wallet = wallet - accessoryPrice[i];
-		}
-		purchase.push(total);
-		balance.push(wallet);
-	}
-	//console.log(balance);
+// for(let i = 0; i < phonePrice.length; i++) {
+// 	let total = phonePrice[i] + accessoryPrice[i];
+// 	// buy phone
+// 	if(phonePrice[i] < wallet) {
+// 		wallet = wallet - phonePrice[i];
+// 		// buy accessory
+// 		if(accessoryPrice[i] < wallet) {
+// 			wallet = wallet - accessoryPrice[i];
+// 		}
+// 		// populate arrays
+// 		purchase.push(total);
+// 		balance.push(wallet);
+// 	}
+// 	//console.log(balance);
+// }
+
+// function calcTotalPurchase () {
+
+// 	let total = 0;
+// 	for(let i = 0; i < purchase.length; i++) {
+
+// 		total += purchase[i];
+// 	}
+// 	return total;
+// }
+
+// let purchaseAmount = calcTotalPurchase();
+// console.log(purchaseAmount);
+
+// let paid = purchaseAmount + (purchaseAmount * TAX_RATE);
+
+// console.log("$" + paid);
+// console.log(balance);
+// console.log(purchase);
+
+var foo = {
+	a: 42
 }
 
-function calcTotalPurchase () {
-	
-	let total = 0;
-	for(let i = 0; i < purchase.length; i++) {
-		
-		total += purchase[i];
-	}
-	return total;
-}
+var bar = Object.create(foo);
 
-let purchaseAmount = calcTotalPurchase();
-console.log(purchaseAmount);
+bar.b = 'King Jesus';
 
-let paid = purchaseAmount + (purchaseAmount * TAX_RATE);
-
-console.log("$" + paid);
-console.log(balance);
-console.log(purchase);
+console.log(foo.b);
