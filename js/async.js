96e835858eb2d6f0a5292bd7ffb0c03790bf5042
wisestMe <wyhzest@gmail.com>
2020-12-26 14:12:55 +0100

    track request state

--- js/async.js
+++ js/async.js
@@ -1,8 +1,8 @@
 const request = new XMLHttpRequest();
 
-// request.addEventListener('readystatechange', () => {
-
-// })
+request.addEventListener('readystatechange', () => {
+  console.log("Track state of request", request, request.readyState)
+})
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();
