207a34c9f4e642b871e4fb769c0afbb770bf7372
wisestMe <wyhzest@gmail.com>
2020-12-26 14:28:39 +0100

    get response text

--- js/async.js
+++ js/async.js
@@ -1,7 +1,11 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-  console.log("Track state of request", request, request.readyState)
+  console.log("Track state of request", request, request.readyState);
+
+  if(request.readyState ===  4) {
+    console.log('I am done', request.responseText);
+  }
 })
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
