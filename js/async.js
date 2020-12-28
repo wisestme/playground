a4280d8016c90fc03e04817d7a3032578b829fed
wisestMe <wyhzest@gmail.com>
2020-12-28 09:19:01 +0100

    convert callback to promise

--- js/async.js
+++ js/async.js
@@ -1,32 +1,26 @@
-const getTodos = (resource, theCallback) => {
-  const request = new XMLHttpRequest();
-
-  request.addEventListener('readystatechange', () => {
-    if(request.readyState ===  4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      theCallback(undefined, data);
-    } else if(request.readyState === 4) {
-      theCallback('could not fetch data', undefined);
-    }
-  });
-
-  request.open('GET', resource);
-  request.send();
-}
-
-// promise example
-const getSomething = () => {
+const getTodos = (resource) => {
   return new Promise((resolve, reject) => {
-    // fetch something
-    //resolve('some data');
-    reject('some error')
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+      if(request.readyState ===  4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if(request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+
+    request.open('GET', resource);
+    request.send();
   })
+  
 }
 
-getSomething().then((data) => {
-  console.log(data);
+getTodos('js/luigi.json').then((data) => {
+  console.log('promise resolved:', data);
 }).catch(err => {
-  console.log(err);
+  console.log('promise rejected:', err);
 });
 // console.log(1);
 // console.log(2);
