2f941837cb9e7ea84eb7c984b103f846deb03f68
wisestMe <wyhzest@gmail.com>
2020-12-29 11:51:55 +0100

    refactor using fetch

--- js/async.js
+++ js/async.js
@@ -1,13 +1,23 @@
-// fetch api
+// async & await
+const getTodos = async () => {
+  const response = await fetch('js/luigi.json');
+  const data = await response.json();
+  return data;
+}
 
-fetch('js/luigi.json').then((response) => {
-  console.log('resolved', response);
-  return response.json();
-}).then((data) => {
-console.log(data);
-}).catch((err) => {
-  console.log('rejected', err);
+getTodos().then(data => {
+  console.log('resolved:', data)
 });
+// // fetch api
+
+// fetch('js/luigi.json').then((response) => {
+//   console.log('resolved', response);
+//   return response.json();
+// }).then((data) => {
+// console.log(data);
+// }).catch((err) => {
+//   console.log('rejected', err);
+// });
 
 // const getTodos = (resource) => {
 //   return new Promise((resolve, reject) => {
