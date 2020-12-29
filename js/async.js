d5637018a1a8cf8215a6e6a146ebf3f4b80c4e00
wisestMe <wyhzest@gmail.com>
2020-12-29 10:32:31 +0100

    asynchronous code using the fetch api

--- js/async.js
+++ js/async.js
@@ -1,46 +1,57 @@
-const getTodos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// fetch api
 
-    request.addEventListener('readystatechange', () => {
-      if(request.readyState ===  4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if(request.readyState === 4) {
-        reject('error getting resource');
-      }
-    });
-
-    request.open('GET', resource);
-    request.send();
-  })
-  
-}
-
-getTodos('js/luigi.json').then((data) => {
-  console.log('promise1 resolved:', data);
-  return getTodos('js/mario.json').then((data) => {
-    console.log('promise2 resolved:', data);
-    return getTodos('js/shaun.json').then((data) => {
-      console.log('promise3 resolved:', data)
-    })
-  })
-}).catch(err => {
-  console.log('promise rejected:', err);
+fetch('js/luigi.json').then((response) => {
+  console.log('resolved', response);
+  return response.json();
+}).then((data) => {
+console.log(data);
+}).catch((err) => {
+  console.log('rejected', err);
 });
-// console.log(1);
-// console.log(2);
 
-// getTodos('js/luigi.json', (err, data) => {
-//   console.log('the call back is being fired');
-//   console.log(data);
-//   getTodos('js/mario.json', (err, data) => {
-//     console.log(data);
-//     getTodos('js/shaun.json', (err, data) => {
-//       console.log(data);
+// const getTodos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
+
+//     request.addEventListener('readystatechange', () => {
+//       if(request.readyState ===  4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if(request.readyState === 4) {
+//         reject('error getting resource');
+//       }
 //     });
-//   });
-// });
 
-// console.log(3);
-// console.log(4);
+//     request.open('GET', resource);
+//     request.send();
+//   })
+  
+// }
+
+// getTodos('js/luigi.json').then((data) => {
+//   console.log('promise1 resolved:', data);
+//   return getTodos('js/mario.json').then((data) => {
+//     console.log('promise2 resolved:', data);
+//     return getTodos('js/shaun.json').then((data) => {
+//       console.log('promise3 resolved:', data)
+//     })
+//   })
+// }).catch(err => {
+//   console.log('promise rejected:', err);
+// });
+// // console.log(1);
+// // console.log(2);
+
+// // getTodos('js/luigi.json', (err, data) => {
+// //   console.log('the call back is being fired');
+// //   console.log(data);
+// //   getTodos('js/mario.json', (err, data) => {
+// //     console.log(data);
+// //     getTodos('js/shaun.json', (err, data) => {
+// //       console.log(data);
+// //     });
+// //   });
+// // });
+
+// // console.log(3);
+// // console.log(4);
