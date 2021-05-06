e14f2ac4b54004bd84ae980098681e9d67f0cc57
wisestme01 <c.nwagwu@genesystechhub.com>
2021-05-06 09:12:07 +0100

    set codition to log result to the console

--- js/async.js
+++ js/async.js
@@ -12,9 +12,13 @@ const request = new XMLHttpRequest(); //XML old data format
 
 request.addEventListener("readystatechange", () => {
   //console.log(request, request.readyState);
-  if(request.readyState === 4)
+  if(request.readyState === 4 && request.status === 200)
   {
-    console.log(request.responseText);
+    console.log(request, request.responseText);
+  }
+  else if(request.readyState === 4)
+  {
+    console.log("Could not fetch the data");
   }
 })
 request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
