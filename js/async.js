7124d78a3f8934434887d287491f882c6a47e517
wisestme01 <c.nwagwu@genesystechhub.com>
2021-05-06 09:40:36 +0100

    set callback condition

--- js/async.js
+++ js/async.js
@@ -6,18 +6,26 @@ const getToDos = (callback) => {
     //console.log(request, request.readyState);
     if(request.readyState === 4 && request.status === 200)
     {
-      callback();
+      callback(undefined, request.responseText);
     }
     else if(request.readyState === 4)
     {
-      callback();
+      callback("Could not fetch data", undefined);
     }
   })
   request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
   request.send();
 }
 
-getToDos(() => {
+getToDos((err, data) => {
   console.log("callback fired");
+  if(err)
+  {
+    console.log(err);
+  }
+  else
+  {
+    console.log(data);
+  }
 });
 
