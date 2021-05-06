09ccb9568c16f2ae58bca23774be0504884e5df3
wisestme01 <c.nwagwu@genesystechhub.com>
2021-05-06 08:42:33 +0100

    send http request

--- js/async.js
+++ js/async.js
@@ -1,67 +1,21 @@
-// async & await
-const getTodos = async () => {
-  const response = await fetch('js/luigi.json');
-  const data = await response.json();
-  return data;
-}
+console.log("Line one");
+console.log("Line two");
+setTimeout(() => {
+  console.log("Callback fired")
+}, 2000)
+console.log("Line three");
+console.log("Line four");
+console.log("Line five");
 
-getTodos().then(data => {
-  console.log('resolved:', data)
-});
-// // fetch api
+// Create request object
+const request = new XMLHttpRequest(); //XML old data format
 
-// fetch('js/luigi.json').then((response) => {
-//   console.log('resolved', response);
-//   return response.json();
-// }).then((data) => {
-// console.log(data);
-// }).catch((err) => {
-//   console.log('rejected', err);
-// });
-
-// const getTodos = (resource) => {
-//   return new Promise((resolve, reject) => {
-//     const request = new XMLHttpRequest();
-
-//     request.addEventListener('readystatechange', () => {
-//       if(request.readyState ===  4 && request.status === 200) {
-//         const data = JSON.parse(request.responseText);
-//         resolve(data);
-//       } else if(request.readyState === 4) {
-//         reject('error getting resource');
-//       }
-//     });
-
-//     request.open('GET', resource);
-//     request.send();
-//   })
-  
-// }
-
-// getTodos('js/luigi.json').then((data) => {
-//   console.log('promise1 resolved:', data);
-//   return getTodos('js/mario.json').then((data) => {
-//     console.log('promise2 resolved:', data);
-//     return getTodos('js/shaun.json').then((data) => {
-//       console.log('promise3 resolved:', data)
-//     })
-//   })
-// }).catch(err => {
-//   console.log('promise rejected:', err);
-// });
-// // console.log(1);
-// // console.log(2);
-
-// // getTodos('js/luigi.json', (err, data) => {
-// //   console.log('the call back is being fired');
-// //   console.log(data);
-// //   getTodos('js/mario.json', (err, data) => {
-// //     console.log(data);
-// //     getTodos('js/shaun.json', (err, data) => {
-// //       console.log(data);
-// //     });
-// //   });
-// // });
-
-// // console.log(3);
-// // console.log(4);
+request.addEventListener("readystatechange", () => {
+  //console.log(request, request.readyState);
+  if(request.readyState === 4)
+  {
+    console.log(request.responseText);
+  }
+})
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+request.send();
