da055d553da0de16432a3dbe7df87716bdb0ee5c
wisestme01 <c.nwagwu@genesystechhub.com>
2021-05-06 09:29:24 +0100

    fire a call back function

--- js/async.js
+++ js/async.js
@@ -1,13 +1,4 @@
-console.log("Line one");
-console.log("Line two");
-setTimeout(() => {
-  console.log("Callback fired")
-}, 2000)
-console.log("Line three");
-console.log("Line four");
-console.log("Line five");
-
-const getToDos = () => {
+const getToDos = (callback) => {
   // Create request object
   const request = new XMLHttpRequest(); //XML old data format
 
@@ -15,16 +6,18 @@ const getToDos = () => {
     //console.log(request, request.readyState);
     if(request.readyState === 4 && request.status === 200)
     {
-      console.log(request, request.responseText);
+      callback();
     }
     else if(request.readyState === 4)
     {
-      console.log("Could not fetch the data");
+      callback();
     }
   })
   request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
   request.send();
 }
 
-getToDos();
+getToDos(() => {
+  console.log("callback fired");
+});
 
