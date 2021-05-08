02e97563747c502f5a5e9307112b2d86e6d2af9d
wisestme01 <c.nwagwu@genesystechhub.com>
2021-05-08 14:33:12 +0100

    chain promises

--- js/async.js
+++ js/async.js
@@ -1,37 +1,42 @@
-const getToDos = (callback) => {
-  // Create request object
-  const request = new XMLHttpRequest(); //XML old data format
 
-  request.addEventListener("readystatechange", () => {
-    //console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200)
-    {
-      const data = JSON.parse(request.responseText)
-      callback(undefined, data);
-    }
-    else if(request.readyState === 4)
-    {
-      callback("Could not fetch data", undefined);
-    }
-  })
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-  request.send();
+
+const getToDos = (resource) => {
+  return new Promise((resolve, reject) => {
+    // Create request object
+    const request = new XMLHttpRequest(); //XML old data format
+
+    request.addEventListener("readystatechange", () => {
+      //console.log(request, request.readyState);
+      if(request.readyState === 4 && request.status === 200)
+      {
+        const data = JSON.parse(request.responseText)
+        resolve(data);
+      }
+      else if(request.readyState === 4)
+      {
+        reject('could not get data');
+      }
+    })
+    request.open("GET", resource);
+    request.send();
+  });
+  
 }
 
 console.log(1);
 console.log(2);
 
-getToDos((err, data) => {
-  console.log("callback fired");
-  if(err)
-  {
-    console.log(err);
-  }
-  else
-  {
-    console.log(data);
-  }
-});
+getToDos('js/todos.json').then(data => {
+  console.log('Promise resolved', data);
+  return getToDos('js/mario.json').then(data => {
+    console.log('Promise 2:', data);
+    return getToDos('js/luigi.json').then(data => {
+      console.log('Promise 3', data);
+    })
+  })
+}).catch(err => {
+  console.log('Promise rejected', err);
+})
 
 console.log(3);
 console.log(4);
